8d4684471c1fc565b470701935f5d1f3d10f701c
あまみや ゆうこ <1@4321.io>
2015-02-14 09:30:00 +0800

    123

---
+++ CodeComb.Yuuko/Owin/OwinExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Owin
+{
+    public static class OwinExtensions
+    {
+        public static IAppBuilder MapYuuko(this IAppBuilder builder)
+        {
+            return builder.Map("/yuuko", app=> {
+                app.Run(async context => {
+                    context.Response.ContentType = "text/plain";
+                    context.Response.WriteAsync("test");
+                });
+            });
+        }
+    }
+}
